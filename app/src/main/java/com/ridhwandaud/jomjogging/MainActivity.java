b91425262ee2e779bbd61b3ba48f922af05d4a18
ridhwan <muhammadridhwandaud@gmail.com>
2017-08-30 22:58:58 +0800

    +add view pager

---
+++ app/src/main/java/com/ridhwandaud/jomjogging/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ridhwandaud.jomjogging;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        NavigationAdapter adapter = new NavigationAdapter(this, getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/ridhwandaud/jomjogging/NavigationAdapter.java
@@ -0,0 +1,51 @@
+package com.ridhwandaud.jomjogging;
+
+import android.app.Notification;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by maideasy on 30/08/2017.
+ */
+
+public class NavigationAdapter extends FragmentPagerAdapter {
+    private Context mContext;
+
+    public NavigationAdapter(Context context,FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new FeedFragment();
+        } else if (position == 1){
+            return new JoggingFragment();
+        }else if (position == 2){
+            return new AccountFragment();
+        }else {
+            return new NotificationsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.tab_notification);
+        } else if (position == 1) {
+            return mContext.getString(R.string.tab_notification);
+        } else if (position == 2) {
+            return mContext.getString(R.string.tab_notification);
+        } else {
+            return mContext.getString(R.string.tab_notification);
+        }
+    }
+}
