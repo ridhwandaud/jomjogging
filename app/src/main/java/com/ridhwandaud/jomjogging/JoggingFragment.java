5aca7c19fae37f006d8a11940254cc20f2a5b5e9
ridhwan <muhammadridhwandaud@gmail.com>
2017-09-03 20:42:53 +0800

    + add google map api and basic my location

--- app/src/main/java/com/ridhwandaud/jomjogging/JoggingFragment.java
+++ app/src/main/java/com/ridhwandaud/jomjogging/JoggingFragment.java
@@ -4,17 +4,30 @@ package com.ridhwandaud.jomjogging;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * A simple {@link Fragment} subclass.
  */
-public class JoggingFragment extends Fragment {
+public class JoggingFragment extends Fragment implements OnMapReadyCallback{
 
+    private GoogleMap mMap;
+    SupportMapFragment mSupportMapFragment;
 
     public JoggingFragment() {
         // Required empty public constructor
@@ -27,8 +40,28 @@ public class JoggingFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.jogging_fragment, container, false);
 
+        initilizeMap();
+
         return rootView;
 
     }
 
+    private void initilizeMap()
+    {
+        mSupportMapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+        if (mSupportMapFragment == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            mSupportMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.map, mSupportMapFragment).commit();
+
+            mSupportMapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+    }
 }
