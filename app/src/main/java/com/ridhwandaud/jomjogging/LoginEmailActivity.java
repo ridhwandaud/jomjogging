44e88baa710dafa6fca625fd0ef41a8ba55695fe
ridhwan <muhammadridhwandaud@gmail.com>
2017-11-17 12:16:12 +0800

    add LoginEmailActivity

--- app/src/main/java/com/ridhwandaud/jomjogging/LoginEmailActivity.java
+++ app/src/main/java/com/ridhwandaud/jomjogging/LoginEmailActivity.java
@@ -16,10 +16,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends BaseActivity implements
-        View.OnClickListener{
+public class LoginEmailActivity extends BaseActivity implements
+        View.OnClickListener {
 
-    private static final String TAG = "LoginActivity";
+
+    private static final String TAG = "LoginEmailActivity";
 
     private EditText mEmailField;
     private EditText mPasswordField;
@@ -29,40 +30,54 @@ public class LoginActivity extends BaseActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_email);
 
         mEmailField = (EditText) findViewById(R.id.field_email);
         mPasswordField = (EditText) findViewById(R.id.field_password);
 
         // Buttons
         findViewById(R.id.email_sign_in_button).setOnClickListener(this);
-        findViewById(R.id.facebook_sign_in_button).setOnClickListener(this);
 
         mAuth = FirebaseAuth.getInstance();
-    }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        login(currentUser);
     }
 
     @Override
     public void onClick(View v) {
         int i = v.getId();
-        if (i == R.id.facebook_sign_in_button) {
-            createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
-        } else if (i == R.id.email_sign_in_button) {
-            signInWithEmail();
+         if (i == R.id.email_sign_in_button) {
+            signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
         }
     }
 
-    private void signInWithEmail(){
-        Intent loginEmailIntent = new Intent(this, LoginEmailActivity.class);
-        startActivity(loginEmailIntent);
-        finish();
+    private void signIn(String email, String password){
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            login(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginEmailActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
     }
 
 
@@ -115,7 +130,7 @@ public class LoginActivity extends BaseActivity implements
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.makeText(LoginEmailActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
                         hideProgressDialog();
